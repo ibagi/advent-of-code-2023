03ee11430d8cb038aa9cc1394ffee6967ae548af
Imre Bagi <bagiimi@gmail.com>
2023-12-17 14:53:12 +0100

    day10 - part two

--- day10/dotnet/Program.cs
+++ day10/dotnet/Program.cs
@@ -1,8 +1,128 @@
 ﻿var input = File.ReadLines("input.txt").ToArray();
 
 Console.WriteLine(PartOne(input));
+Console.WriteLine(PartTwo(input));
 
-static Position FindStart(string[] map)
+static int PartOne(string[] map)
+{
+    var loop = FindLoop(map);
+    return (int)Math.Ceiling(loop.Count() / 2d);
+}
+
+static int PartTwo(string[] map)
+{
+    var loop = FindLoop(map).ToArray();
+    var polygon = GetLoopPolygon(loop).ToArray();
+    var loopPoints = new HashSet<Coordinates>(loop);
+
+    var area = 0;
+    var minX = loop.Min(_ => _.X);
+    var maxX = loop.Max(_ => _.X);
+
+    for (var y = loop.Min(_ => _.Y); y <= loop.Max(_ => _.Y); y++)
+    {
+        for (var x = minX; x < maxX; x++)
+        {
+            var point = new Coordinates(x, y);
+            if (!loopPoints.Contains(point) && IsInPolygon(point, polygon))
+            {
+                area++;
+            }
+        }
+    }
+
+    return area;
+}
+
+
+static bool IsInPolygon(Coordinates point, Coordinates[] polygon)
+{
+    var hits = 0;
+    var x = point.X;
+    var y = point.Y;
+
+    for (int i = 0; i < polygon.Length - 1; i++)
+    {
+        var x1 = polygon[i].X;
+        var x2 = polygon[i + 1].X;
+        var y1 = polygon[i].Y;
+        var y2 = polygon[i + 1].Y;
+
+        if (y < y1 != y < y2 && x < (x2 - x1) * (y - y1) / (y2 - y1) + x1)
+        {
+            hits++;
+        }
+    }
+
+    return hits % 2 != 0;
+}
+
+static IEnumerable<Coordinates> GetLoopPolygon(Coordinates[] loop)
+{
+    var direction = loop[1].Direction(loop[0]);
+    var start = 0;
+
+    for (int i = 0; i < loop.Length; i++)
+    {
+        if (loop[i + 1].Direction(loop[i]) != direction)
+        {
+            start = i;
+            break;
+        }
+    }
+
+    for (int i = start; i < loop.Length; i++)
+    {
+        var d = loop[i].Direction(loop[i - 1]);
+
+        if (d != direction)
+        {
+            yield return loop[i - 1];
+        }
+
+        direction = d;
+    }
+}
+
+static IEnumerable<Coordinates> FindLoop(string[] map)
+{
+    var startingNode = FindStart(map);
+    var queue = new Queue<Pipe>();
+    var seen = new HashSet<Pipe>();
+
+    var startingPipe = Neighbors(startingNode)
+        .Select(x => PipeAt(map, x))
+        .Where(x => x.IsConnectedWith(startingNode))
+        .FirstOrDefault();
+
+    queue.Enqueue(startingPipe!);
+    var points = new List<Coordinates>();
+
+    while (queue.Any())
+    {
+        var pipe = queue.Dequeue();
+        seen.Add(pipe);
+
+        var a = PipeAt(map, pipe.A);
+        var b = PipeAt(map, pipe.B);
+
+        if (!seen.Contains(a) && a != Pipe.None)
+        {
+            yield return pipe.A;
+            queue.Enqueue(a);
+        }
+
+        if (!seen.Contains(b) && b != Pipe.None)
+        {
+            yield return pipe.B;
+            queue.Enqueue(b);
+        }
+    }
+
+    yield return startingNode;
+}
+
+static Coordinates FindStart(string[] map)
 {
     for (var y = 0; y < map.Length; y++)
     {
@@ -18,45 +138,7 @@ static Position FindStart(string[] map)
     throw new Exception("Invalid map!");
 }
 
-static int PartOne(string[] map)
-{
-    var startingNode = FindStart(map);
-    var queue = new Queue<Pipe>();
-    var seen = new HashSet<Pipe>();
-    var steps = 1;
-
-    var startingPipe = Neighbors(startingNode)
-        .Select(x => PipeAt(map, x))
-        .Where(x => x.IsConnectedWith(startingNode))
-        .FirstOrDefault();
-
-    queue.Enqueue(startingPipe!);
-
-    while (queue.Any())
-    {
-        var pipe = queue.Dequeue();
-        seen.Add(pipe);
-
-        var a = PipeAt(map, pipe.A);
-        var b = PipeAt(map, pipe.B);
-
-        if (!seen.Contains(a) && a != Pipe.None)
-        {
-            queue.Enqueue(a);
-        }
-
-        if (!seen.Contains(b) && b != Pipe.None)
-        {
-            queue.Enqueue(b);
-        }
-
-        steps++;
-    }
-
-    return steps / 2;
-}
-
-static IEnumerable<Position> Neighbors(Position pos)
+static IEnumerable<Coordinates> Neighbors(Coordinates pos)
 {
     yield return pos with { X = pos.X, Y = pos.Y - 1 };
     yield return pos with { X = pos.X, Y = pos.Y + 1 };
@@ -64,8 +146,18 @@ static IEnumerable<Position> Neighbors(Position pos)
     yield return pos with { X = pos.X - 1, Y = pos.Y };
 }
 
-static Pipe PipeAt(string[] map, Position pos)
+static Pipe PipeAt(string[] map, Coordinates pos)
 {
+    if (pos.Y < 0 || pos.Y >= map.Length)
+    {
+        return Pipe.None;
+    }
+
+    if (pos.X < 0 || pos.X >= map[0].Length)
+    {
+        return Pipe.None;
+    }
+
     var value = map[pos.Y][pos.X];
     return value switch
     {
@@ -79,11 +171,16 @@ static Pipe PipeAt(string[] map, Position pos)
     };
 }
 
-record Position(int X, int Y);
-record Pipe(Position A, Position B)
+record Coordinates(int X, int Y)
+{
+    public Coordinates Direction(Coordinates point) =>
+        new(X - point.X, Y - point.Y);
+}
+
+record Pipe(Coordinates A, Coordinates B)
 {
     public static Pipe None =>
-        new(new Position(-1, -1), new Position(-1, -1));
+        new(new Coordinates(-1, -1), new Coordinates(-1, -1));
 
-    public bool IsConnectedWith(Position pos) => A == pos || B == pos;
+    public bool IsConnectedWith(Coordinates pos) => A == pos || B == pos;
 }
